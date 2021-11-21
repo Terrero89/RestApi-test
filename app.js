03f8b8423af927f5fc50d9c29034661ce35b2241
Sergio Terrero <sergio-terrero@hotmail.com>
2021-11-20 21:53:23 -0500

    setup server and db

---
+++ app.js
@@ -0,0 +1,21 @@
+
+//*SETUP FOR DEPENDENCIES 
+
+const express = require('express')
+const bodyParser = require('body-parser')
+const ejs = require('ejs')
+const mongoose = require('mongoose')
+
+const app = express()
+
+app.set('view engine', 'ejs')
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(express.static('public'))
+
+//*TODO
+
+app.listen(3000, ()=>{
+    console.log('Server running on port 3000')
+})
+
+
